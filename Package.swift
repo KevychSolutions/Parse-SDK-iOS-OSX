c1694a0f19a4cbc500854ac6cfe06d51d65e16cb
artempolivodskyi <artem.polivodsky@kevychsolutions.com>
2022-11-22 15:10:05 +0200

    Upadate package file

--- Package.swift
+++ Package.swift
@@ -3,14 +3,18 @@ import PackageDescription
 
 let package = Package(
     name: "Parse_SDK",
+    platforms: [
+        .iOS(.v9),
+    ],
     products: [
         .library(
             name: "Parse",
             targets: ["Parse-iOS"])
     ],
-    dependencies: [
-        // no dependencies
-    ],
+    // dependencies: [
+    //     // no dependencies
+    // ],
+    
     targets: [
         .target(
             name: "Parse-iOS",
