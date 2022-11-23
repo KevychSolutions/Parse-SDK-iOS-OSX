cf655514fd7f0c9f9cee2ca54e6e908fce300e67
Andriy Hrytsyshyn <andriy481516@gmail.com>
2022-11-23 09:54:54 +0200

    fix

--- Package.swift
+++ Package.swift
@@ -5,12 +5,6 @@ import PackageDescription
 
 let package = Package(
     name: "Parse-SDK-iOS-OSX",
-    products: [
-        .library(
-            name: "Parse-iOS",
-            targets: ["Parse-iOS"]
-        )
-    ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
