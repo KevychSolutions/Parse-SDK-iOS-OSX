ce7720fdbeb8fca4564420f59d2a719b821e9b8f
artempolivodskyi <artem.polivodsky@kevychsolutions.com>
2022-11-25 17:30:19 +0200

    update

--- Package.swift
+++ Package.swift
@@ -22,14 +22,16 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(path: "../Carthage/Checkouts/Bolts-ObjC/Bolts")
+        .package(name: "Bolts", url: "https://github.com/BoltsFramework/Bolts-Swift", .branch("main"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Parse-iOS",
-            dependencies: [],
+            dependencies: [
+                .product(name: "RswiftDynamic", package: "Bolts")
+            ],
             path: "Parse/Parse",
             publicHeadersPath: ".",
             cSettings: [
