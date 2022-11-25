f92e5ed12890509be5aef64bf479a74ea0c6813e
artempolivodskyi <artem.polivodsky@kevychsolutions.com>
2022-11-25 17:11:01 +0200

    test update

--- Package.swift
+++ Package.swift
@@ -17,32 +17,18 @@ let package = Package(
             name: "Parse",
             type: .dynamic,
             targets: ["Parse-iOS"]
-        ),
-        .library(
-            name: "Parse-iOS-Dynamic",
-            type: .dynamic,
-            targets: ["Parse-iOS-Dynamic"]
         )
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
+        .package(path: "Carthage/Checkouts/Bolts-ObjC")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Parse-iOS",
-            dependencies: [],
-            path: "Parse/Parse",
-            publicHeadersPath: ".",
-            cSettings: [
-                .headerSearchPath("."),
-                .headerSearchPath("Private"),
-            ]
-        ),
-        .target(
-            name: "Parse-iOS-Dynamic",
             dependencies: [],
             path: "Parse/Parse",
             publicHeadersPath: ".",
