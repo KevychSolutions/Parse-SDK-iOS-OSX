f59b75a9a1b981846c1832d9061f79b39876407b
Andriy Hrytsyshyn <andriy481516@gmail.com>
2022-11-25 16:08:17 +0200

    update package

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "Parse-SDK-iOS-OSX",
     defaultLocalization: "en",
-     platforms: [
+    platforms: [
         .macOS(.v10_15),
         .iOS(.v12),
         .tvOS(.v12),
@@ -15,6 +15,7 @@ let package = Package(
     products: [
         .library(
             name: "Parse",
+            type: .dynamic,
             targets: ["Parse-iOS"]
         ),
     ],
@@ -26,10 +27,12 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "Parse-iOS",
-            dependencies: [],
-            path: "Parse/Parse"
-        )
+            path: "LNPopupController",
+            publicHeadersPath: ".",
+            cSettings: [
+                .headerSearchPath("."),
+                .headerSearchPath("Private"),
+            ]),
         // .executableTarget(
         //     name: "Parse-SDK-iOS-OSX",
         //     dependencies: []),
