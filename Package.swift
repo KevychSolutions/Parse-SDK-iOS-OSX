5e280661b97c684383c95a1f0c81b53da236b3af
Andriy Hrytsyshyn <andriy481516@gmail.com>
2022-11-25 16:12:26 +0200

    update package

--- Package.swift
+++ Package.swift
@@ -27,12 +27,14 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            path: "LNPopupController",
+            name: "Parse-iOS",
+            dependencies: [],
+            path: "Parse",
             publicHeadersPath: ".",
             cSettings: [
                 .headerSearchPath("."),
                 .headerSearchPath("Private"),
-            ]),
+            ])
         // .executableTarget(
         //     name: "Parse-SDK-iOS-OSX",
         //     dependencies: []),
