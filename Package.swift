e5a62cc8376c5efeb904b564b31164808e182381
artempolivodskyi <artem.polivodsky@kevychsolutions.com>
2022-11-26 12:27:13 +0200

    update

--- Package.swift
+++ Package.swift
@@ -43,7 +43,7 @@ let package = Package(
         .target(
             name: "Parse-iOS",
             dependencies: [
-                .product(name: "Bolts-ObjC", package: "Bolts-ObjC")
+                .product(name: "Bolts-iOS", package: "Bolts-iOS")
             ],
             path: "Parse/Parse",
             publicHeadersPath: ".",
